81a1c47851b1b9ff2f6338a79f1962bd4733dafb
Chinmaya57 <93158226+Chinmaya57@users.noreply.github.com>
2021-10-28 11:31:33 +0530

    Add files via upload

---
+++ config-server/src/test/java/com/chintu/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chintu.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
